0f185a934adad3596a61006080a10843c9331701
Zori <list_babayan@mail.ru>
2022-01-21 01:52:24 +0300

    ADD Crud  create.php

---
+++ crud/create.php
@@ -0,0 +1,9 @@
+<?php
+require_once '../config/connect.php';
+$email = $_POST['email'];
+$phone = $_POST['phone'];
+$name = $_POST['name'];
+
+mysqli_query($connect, "INSERT INTO `feedback` (`id`, `email`, `phone`, `name`) VALUES (NULL, '$email', '$phone', '$name')");
+
+header('Location: /');
